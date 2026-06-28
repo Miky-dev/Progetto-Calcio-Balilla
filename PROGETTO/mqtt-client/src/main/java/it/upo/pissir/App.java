a8969f4f365a7a3ed45c29012cdbb8354ce62261
Michele Vitella <miky.vitella@gmail.com>
2026-06-28 11:25:00 +0200

    Implementazione e test funzionamento mosquitto

---
+++ PROGETTO/mqtt-client/src/main/java/it/upo/pissir/App.java
@@ -0,0 +1,13 @@
+package it.upo.pissir;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ PROGETTO/mqtt-client/src/main/java/it/upo/pissir/SubscribeCallback.java
@@ -0,0 +1,21 @@
+package it.upo.pissir;
+
+import org.eclipse.paho.client.mqttv3.*;
+
+public class SubscribeCallback implements MqttCallback {
+
+    @Override
+    public void connectionLost(Throwable cause) {
+        System.out.println("Connessione persa: " + cause.getMessage());
+    }
+
+    @Override
+    public void messageArrived(String topic, MqttMessage message) throws Exception {
+        System.out.println("Messaggio ricevuto - Topic: " + topic + " | Payload: " + message.toString());
+    }
+
+    @Override
+    public void deliveryComplete(IMqttDeliveryToken token) {
+        System.out.println("Messaggio consegnato.");
+    }
+}
